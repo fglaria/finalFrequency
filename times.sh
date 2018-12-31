c2f228b2b5f2eeca83753a335b724261fea535b9
fglaria <fglaria@gmail.com>
2018-12-31 11:39:12 -0300

    Times script

--- times.sh
+++ times.sh
@@ -1,19 +1,29 @@
 #! /bin/bash
 # set -x
 
-while getopts "i:c" option; do
+while getopts "i:n:r:c" option; do
     case $option in
         i)
             route=$OPTARG
             ;;
+	n)
+	    nodes=$OPTARG
+	    ;;
+	r)
+	    random=$OPTARG
+	    ;;
         c) 
             g++ -std=c++11 times.cpp -o times -I ~/include -L ~/lib -lsdsl -ldivsufsort -ldivsufsort64 -O3 -DNDEBUG
             ;;
     esac
 done
 
-if [[ $route ]]; then
-    ./times $route > $route.log 2> $route.error
-else
+if [ -z "$route" ]; then
     echo "No path to files"
+elif [ -z "$nodes" ]; then
+    echo "No number of nodes"
+elif [ -z "$random" ]; then
+    echo "No random specified"
+else
+    ./times $route $nodes $random > $route.log 2> $route.error
 fi
